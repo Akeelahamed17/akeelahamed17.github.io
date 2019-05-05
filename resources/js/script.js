b868a9259759fee4d913c29db54c90efce6fca10
Akeel Ahamed <aqeelahamed17@gmail.com>
2019-05-05 12:42:15 +0100

    made smooth animations

--- resources/js/script.js
+++ resources/js/script.js
@@ -13,11 +13,7 @@ $(document).ready(function () {
     /* Navigation Scroll */
     /* https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
     // Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
+$('a[href*="#"]:not([href="#"])').click(function(event) {
     // On-page links
     if (
       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
